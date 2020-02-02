d4222fb5dc896d567f6cd239e6dace70a09691ac
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-03 04:02:43 +0900

    Удаление Home Bridge и его хвостов

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+sudo killall homebridge
+sudo userdel -r homebridge
+sudo npm uninstall -g homebridge
+sudo apt-get purge --auto-remove nodejs -y </dev/null
+sudo apt-get purge --auto-remove libavahi-compat-libdnssd-de -y </dev/null
+sudo rm -rf /usr/lib/node_modules/homebridge*
+sudo rm -rf /etc/systemd/system/homebridge
+sudo rm -rf /etc/default/homebridge*
+sudo rm -rf /home/pi/.homebridge*
+sudo rm -rf /var/lib/homebridge*
+sudo rm -rf ~/.homebridge*
+sudo rm -rf /usr/bin/ps4-waker
+sudo rm -rf /usr/lib/node_modules/ps4-waker
