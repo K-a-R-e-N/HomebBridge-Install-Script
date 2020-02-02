2c3e96b18d3a0672434cec05fee4daa83831c259
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-03 07:00:31 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,5 @@ echo Установка Home Bridge и его зависимостей
 echo =========================================
 echo -en '\n'
 echo # # Установка репозитория для Node.js 12.x...
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash - > /dev/null 2>&1
-echo -en '\n'
-echo # # Установка пакетов nodejs gcc g++ make python...
-sudo apt-get install -y nodejs gcc g++ make python > /dev/null 2>&1
-echo Версия установленой NodeJS: && sudo node -vecho
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -> /dev/null 2>&1
 echo -en '\n'
