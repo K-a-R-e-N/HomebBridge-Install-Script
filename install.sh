73c9c5766986fbbd1fad68383761815c315c49f3
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-03 07:06:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 echo -en '\n'
 echo '========================================='
-echo 'Установка Home Bridge и его зависимостей'
+echo ' Установка Home Bridge и его зависимостей'
 echo '========================================='
 echo -en '\n'
 echo '# # Установка репозитория для Node.js 12.x...'
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -> /dev/null 2>&1
+echo -en '\n'
