702f4b6ec00cdb32adf72b1dd240d8e6734d086f
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-03 05:28:03 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,17 +3,19 @@
 echo ====================================
 echo  Удаление Home Bridge и его хвостов
 echo ====================================
-echo Если запушен Home Bridge, то убываем процесс...
+echo
+echo Убываем процесс...
 sudo killall homebridge > /dev/null 2>&1
-echo Удаляем пользователя homebridge, если есть таковой...
+echo Удаляем пользователя homebridge...
 sudo userdel -r homebridge > /dev/null 2>&1
-echo Деинсталируем Home Bridge, если был установлен...
+echo Деинсталируем Home Bridge...
 sudo npm uninstall -g homebridge > /dev/null 2>&1
-echo Деинсталируем nodejs, если был установлен...
+echo Деинсталируем nodejs...
 sudo apt-get purge --auto-remove nodejs -y > /dev/null 2>&1
-echo Деинсталируем libavahi-compat-libdnssd-dev, если был установлен...
+echo Деинсталируем libavahi-compat-libdnssd-dev...
 sudo apt-get purge --auto-remove libavahi-compat-libdnssd-dev -y
-echo Очишаем хвосты...
+echo
+echo Очишаем хвосты:
 echo по пути /usr/lib/node_modules/homebridge*
 sudo rm -rf /usr/lib/node_modules/homebridge*
 echo по пути /etc/systemd/system/homebridge
@@ -28,13 +30,17 @@ echo по пути /home/homebridge*
 sudo rm -rf /home/homebridge*
 echo по пути ~/.homebridge*
 sudo rm -rf ~/.homebridge*
-echo Избавляемся от плагинов ...
+echo
+echo Очищаем хвосты от плагинов:
 echo Плагин ps4-waker
 sudo rm -rf /usr/bin/ps4-waker
 sudo rm -rf /usr/lib/node_modules/ps4-waker
+echo
 echo ========================================================
 echo  Процесс удаления Home Bridge и его хвостов, завершен !
 echo ========================================================
+echo
 echo Самоудаляемся...
 cd ..	
 sudo rm -rf HomebBridge-Install-Script
+cd ~
