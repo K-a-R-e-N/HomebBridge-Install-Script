e9fcd80748bf7f3db792bd0ac149e7438d6b4f6e
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-02-06 16:35:44 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,59 +1,61 @@
 #!/bin/bash
 
-echo -en '\n'
-echo ' ========================================================'
-echo '            Удаление Home Bridge и его хвостов'
-echo ' ========================================================'
-echo -en '\n'
-echo '   Убиваем процесс Homebridge...'
+echo -en "\n"
+echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+echo "                       Удаление HomeBridge и его хвостов"
+echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+echo -en "\n"
+
+echo -en "\n" ; echo "# # Убиваем процесс Homebridge..."
 sudo killall  -w -s 9 -u homebridge > /dev/null 2>&1
-echo -en '\n'
-echo '   Деинсталлируем службы Homebridge...'
+
+echo -en "\n" ; echo "# # Деинсталлируем службы Homebridge..."
 sudo hb-service uninstall > /dev/null 2>&1
-echo -en '\n'
-echo '   Деинсталлируем HomeBridge...'
+
+echo -en "\n" ; echo "# # Деинсталлируем HomeBridge..."
 sudo npm uninstall -g homebridge > /dev/null 2>&1
-echo -en '\n'
-echo '   Деинсталлируем интерфейс Homebridge Config UI X...'
+
+echo -en "\n" ; echo "# # Деинсталлируем интерфейс Homebridge Config UI X..."
 sudo npm uninstall -g homebridge-config-ui-x > /dev/null 2>&1
-echo -en '\n'
-echo '   Деинсталлируем NodeJS...'
+
+echo -en "\n" ; echo "# # Деинсталлируем NodeJS..."
 sudo apt-get purge --auto-remove nodejs -y > /dev/null 2>&1
-echo -en '\n'
-echo '   Удаляем пользователя homebridge...'
+
+echo -en "\n" ; echo "# # Удаляем пользователя homebridge..."
 sudo userdel -rf homebridge > /dev/null 2>&1
 
-echo -en '\n'
-echo '   Очищаем хвосты сервисов:'
-echo '       по пути /etc/systemd/system/homebridge*'
-echo '       по пути /etc/systemd/system/multi-user.target.wants/homebridge*'
+
+echo -en "\n" ; echo "# #  Очищаем хвосты сервисов:"
+echo '       по пути /etc/systemd/system/homebridge*"
+echo '       по пути /etc/systemd/system/multi-user.target.wants/homebridge*"
 sudo rm -rf /etc/systemd/system/homebridge*
 sudo rm -rf /etc/systemd/system/multi-user.target.wants/homebridge*
 echo -en '\n'
 
-echo '   Очищаем хвосты, для возможности последующей нормальной установки:'
-echo '       по пути /usr/lib/node_modules/homebridge*'
+
+echo -en "\n" ; echo "# # Очищаем хвосты, для возможности последующей нормальной установки:"
+echo "   - по пути /usr/lib/node_modules/homebridge*"
+echo "   - по пути /usr/bin/homebridge"
+echo "   - по пути /etc/default/homebridge*"
+echo "   - по пути /var/lib/homebridge*"
+echo "   - по пути /home/pi/.homebridge*"
+echo "   - по пути /home/homebridge*"
+echo "   - по пути ~/.homebridge*"
 sudo rm -rf /usr/lib/node_modules/homebridge*
-echo '       по пути /usr/bin/homebridge'
 sudo rm -rf /usr/bin/homebridge*
-echo '       по пути /etc/default/homebridge*'
 sudo rm -rf /etc/default/homebridge*
-echo '       по пути /var/lib/homebridge*'
 sudo rm -rf /var/lib/homebridge*
-echo '       по пути /home/pi/.homebridge*'
 sudo rm -rf /home/pi/.homebridge*
-echo '       по пути /home/homebridge*'
 sudo rm -rf /home/homebridge*
-echo '       по пути ~/.homebridge*'
 sudo rm -rf ~/.homebridge*
 
-echo -en '\n'
-echo '   Очищаем хвосты от плагинов:'
-echo '       Плагин ps4-waker'
+echo -en "\n" ; echo "# # Очищаем хвосты от плагинов:"
+echo "   - Плагин ps4-waker"
 sudo rm -rf /usr/bin/ps4-waker
 sudo rm -rf /usr/lib/node_modules/ps4-waker
-echo -en '\n'
-echo ' ========================================================='
-echo '    Процесс удаления Home Bridge и его хвостов завершен'
-echo ' ========================================================='
-echo -en '\n'
+
+echo -en "\n"
+echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+echo "               Процесс удаления HomeBridge и его хвостов завершен"
+echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+echo -en "\n"
