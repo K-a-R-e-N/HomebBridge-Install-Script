24a4824d71c1e3c98b21c8c5d4628c68d253d3d2
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-07-22 23:53:38 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -92,116 +92,19 @@ echo -en "\n" ; echo "  # # Установка интерфейса для HomeB
 sudo npm install -g --unsafe-perm homebridge-config-ui-x > /dev/null
 
 echo -en "\n" ; echo "  # # Создание основного пользователя для HomeBridge..."
-sudo useradd -rm homebridge -G dialout,gpio,i2c > /dev/null
-
 echo -en "\n" ; echo "  # # Добавление полномочия по управлению через интерфейс..."
-sudo grep homebridge /etc/sudoers > /dev/null || echo 'homebridge    ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo > /dev/null
-
 echo -en "\n" ; echo "  # # Создание основного каталога HomeBridge..."
-[ ! -d ~/.homebridge ] && sudo mkdir -p ~/.homebridge
-sudo chown -R homebridge: ~/.homebridge > /dev/null
-
 echo -en "\n" ; echo "  # # Создание конфигурационного файла HomeBridge..."
-sudo rm -rf ~/.homebridge/config.json
-sudo tee -a ~/.homebridge/config.json > /dev/null <<_EOF_
-{
-    "bridge": {
-        "name": "Homebridge",
-        "username": "CB:22:3D:E2:CE:31",
-        "port": 51826,
-        "pin": "432-11-234"
-    },
-    "accessories": [],
-    "platforms": [
-        {
-            "platform": "config",
-            "name": "Config",
-            "port": 8080,
-            "auth": "form",
-            "standalone": true,
-            "restart": "sudo -n systemctl restart homebridge homebridge-config-ui-x",
-            "sudo": true,
-            "log": {
-                "method": "systemd",
-                "service": "homebridge"
-            }
-        }
-    ]
-}
-_EOF_
-
 echo -en "\n" ; echo "  # # Создание служб автозапуска..."
-sudo rm -rf /etc/systemd/system/homebridge.service
-sudo tee -a /etc/systemd/system/homebridge.service > /dev/null <<_EOF_
-[Unit]
-Description=Homebridge
-After=syslog.target network-online.target
-
-[Service]
-Type=simple
-User=homebridge
-EnvironmentFile=/etc/default/homebridge
-ExecStart=$(which homebridge) \$HOMEBRIDGE_OPTS
-Restart=on-failure
-RestartSec=3
-KillMode=process
-CapabilityBoundingSet=CAP_IPC_LOCK CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW CAP_SETGID CAP_SETUID CAP_SYS_CHROOT CAP_CHOWN CAP_FOWNER CAP_DAC_OVERRIDE CAP_AUDIT_WRITE CAP_SYS_ADMIN
-AmbientCapabilities=CAP_NET_RAW
-
-[Install]
-WantedBy=multi-user.target
-_EOF_
-
-sudo rm -rf /etc/systemd/system/homebridge-config-ui-x.service
-sudo tee -a /etc/systemd/system/homebridge-config-ui-x.service > /dev/null <<_EOF_
-[Unit]
-Description=Homebridge Config UI X
-After=syslog.target network-online.target
-
-[Service]
-Type=simple
-User=homebridge
-EnvironmentFile=/etc/default/homebridge
-ExecStart=$(which homebridge-config-ui-x) \$HOMEBRIDGE_OPTS
-Restart=on-failure
-RestartSec=3
-KillMode=process
-CapabilityBoundingSet=CAP_IPC_LOCK CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW CAP_SETGID CAP_SETUID CAP_SYS_CHROOT CAP_CHOWN CAP_FOWNER CAP_DAC_OVERRIDE CAP_AUDIT_WRITE CAP_SYS_ADMIN
-AmbientCapabilities=CAP_NET_RAW
-
-[Install]
-WantedBy=multi-user.target
-_EOF_
-
 echo -en "\n" ; echo "  # # Создаем файл настроек HomeBridge..."
-sudo rm -rf /etc/default/homebridge
-sudo tee -a /etc/default/homebridge > /dev/null <<_EOF_
-# Defaults / Configuration options for homebridge
-# The following settings tells homebridge where to find the config.json file and where to persist the data (i.e. pairing and others)
-HOMEBRIDGE_OPTS=-U $HOME/.homebridge -I
-
-# If you uncomment the following line, homebridge will log more
-# You can display this via systemd's journalctl: journalctl -f -u homebridge
-# DEBUG=*
-
-# To enable web terminals via homebridge-config-ui-x uncomment the following line
-# HOMEBRIDGE_CONFIG_UI_TERMINAL=1
-_EOF_
+echo -en "\n" ; echo "  # # Добавление служб в список автозагрузки и их запуск..."
+sudo hb-service install --user homebridge
 
 if [ -d ~/HB_BackUp/ ]; then 
 echo -en "\n" ; echo "  # # Восстанавление резервной копии конфигурационного файла HomeBridge..."
 sudo mv -f ~/HB_BackUp/config.json.* ~/.homebridge/
 sudo rm -rf ~/HB_BackUp
 fi
-
-echo -en "\n" ; echo "  # # Добавление служб в список автозагрузки и их запуск..."
-sudo systemctl -q daemon-reload
-sudo systemctl -q enable homebridge
-sudo systemctl -q start homebridge
-sudo systemctl -q enable homebridge-config-ui-x
-sudo systemctl -q start homebridge-config-ui-x
-
-
 echo -en "\n"
 echo -en "\n"
 echo -en "\n"
