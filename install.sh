b6b219e39bff05cbe2cb4930f547b55131fedfa0
FOR PERFECT THOUGHTS <60580344+K-a-R-e-N@users.noreply.github.com>
2020-07-23 01:26:45 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,19 +27,7 @@ function GoToMenu {
   read key
   printf "\n"
   case $key in
-  1)     echo "                     - Предварительная очистка системы..." && sleep 2 && clear && bash uninstall.sh && Zagolovok
-                                            echo -en "\n" ; echo "  # # Test..."
-                                            if [ -f ~/.homebridge/config.json ]; then
-                                            echo -en "\n" ; echo "  # # Создание резервной копии конфигурационного файла HomeBridge..."
-                                            sudo mkdir -p ~/HB_BackUp && sudo chmod 777 ~/HB_BackUp
-                                            sudo cp -f ~/.homebridge/config.json ~/HB_BackUp/config.json.$(date +%s)000
-                                            if [ -f /var/lib/homebridge/config.json ]; then
-                                            echo -en "\n" ; echo "  # # Создание резервной копии конфигурационного файла HomeBridge..."
-                                            sudo mkdir -p ~/HB_BackUp && sudo chmod 777 ~/HB_BackUp
-                                            sudo cp -f /var/lib/homebridge/config.json ~/HB_BackUp/config.json.$(date +%s)000
-                                            
-                                            echo -en "\n" ; echo "  # # Test2..."
-                                            return
+  1)     echo "                     - Предварительная очистка системы..." && sleep 2 && clear && bash uninstall.sh && Zagolovok && return;;
   2)     echo "                  - Выполнение скрипта без очистки системы..." && sleep 2 && clear && Zagolovok
                                             if [ -f ~/.homebridge/config.json ]; then
                                             echo -en "\n" ; echo "  # # Создание резервной копии конфигурационного файла HomeBridge..."
